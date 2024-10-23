1551f0b20886b6b1da3af1e751128bfdfbc914c8
hernann <hernann@sprbun.com>
2024-10-23 16:51:16 -0500

    crud Python

--- Persona.py
+++ Persona.py
@@ -1,9 +1,11 @@
 class Persona:
     def __init__(self,personaId=None,nombre=None,apellido=None,direccion=None,ciudad=None):
-        self.personaId=personaId
+        self.__personaId=personaId
         self.nombre=nombre
         self.apellido=apellido
         self.direccion=direccion
         self.ciudad=ciudad
+    def get_personaId(self):
+        return self.__personaId
     def __str__(self):
-        return f''' id: {self.personaId}, nombre: {self.nombre}, apellido: {self.apellido} '''
+        return f''' id: {self.get_personaId()}, nombre: {self.nombre}, apellido: {self.apellido} '''
